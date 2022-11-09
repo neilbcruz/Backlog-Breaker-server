a6af7b8f1aa69ed3cbce1f9479ff26f122c78627
Neil Cruz <neilbcruz@gmail.com>
2022-11-09 18:05:35 -0500

    fix user endpoint and remove some files

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const app = express();
 const jwt = require('jsonwebtoken');
-const fs = require("fs");
 
 require('dotenv').config();
 
@@ -21,13 +20,12 @@ app.use('/games', gamesRoute)
 const profileRoute = require('./routes/profileRoute');
 app.use('/profile', profileRoute)
 
-const secretKey = 'a752e1933bee9144efbdbeff2b831345f8843c125ce6af70840f4106ef6fad90'
+const secretKey = process.env.SECRET_KEY;
 
 function authorize(req, res, next) {
 
   const {authorization} = req.headers
   const token = authorization.split(" ")[1];
-  // console.log(req.headers)
 
   jwt.verify(token, secretKey, (err, decoded) => {
     if (err) {
@@ -35,35 +33,29 @@ function authorize(req, res, next) {
       .json({ success: false, message: 'No Token.'})
     } else {
       req.decoded = decoded;
-      
     }
     next()
   })
 }
 
-const users = JSON.parse(fs.readFileSync('./data/users.json'));
-
-const getUser = (username) => users.find((user) => user.username === username);
+const users = {}
 
 app.post('/register', (req, res) => {
-  // const { username, name, password } = req.body;
-  // users[username] = {
-  //   name,
-  //   password
-  // };
-  // res.json({ success: 'true' });
-  users.push(req.body);
-  res.json({ success: true });
+  const { username, name, password } = req.body;
+  users[username] = {
+    name,
+    password
+  };
+  res.json({ success: 'true' });
 });
 
 // Login end point
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  // const user = users[username];
-  const user = getUser(username)
+  const user = users[username];
 
   if (!!user && user.password === password) {
-      const token = jwt.sign({ name: user.name }, secretKey);
+      const token = jwt.sign({ name: user.name, username }, secretKey);
       res.json({token})
   } else {
     res.status(401).json({
